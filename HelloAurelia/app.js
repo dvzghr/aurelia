9c4428c0f43798b6a6b104d66061aca6171497ff
thshr <ths_hr@hotmail.com>
2015-06-29 23:54:23 +0200

    initial

---
+++ HelloAurelia/app.js
@@ -0,0 +1,17 @@
+﻿export class App{
+
+    constructor() {
+        this.message = '';
+        this.counter = 0;
+    }
+
+    activate(){
+        this.message = 'Hello from Aurelia! :)';
+    }
+
+    changeMessage(){
+        this.message += ' LORENA';
+        alert('counter: ' + ++this.counter);
+    }
+
+}
---
+++ HelloAurelia/greeter.js
@@ -0,0 +1,6 @@
+﻿var message = "Hello world!";
+
+export function sayHello(msg) {
+    if (!msg) msg = message;
+    alert(msg);
+}
